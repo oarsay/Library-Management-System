b5794bdebc2015b1f98d2763041d36276b355b61
oguz <oguz@DESKTOP-MOG6CAO>
2022-05-25 05:29:20 -0700

    Add project files.

---
+++ Library/LibraryDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext() { }
+
+        public LibraryDbContext(DbContextOptions options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            SeedInitialAssetStatuses(modelBuilder);
+            LinkAssets(modelBuilder);
+            LinkAssetTags(modelBuilder);
+        }
+
+        private static void LinkAssets(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        private static void LinkAssetTags(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        private static void SeedInitialAssetStatuses(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id{ get; set;}
+        public string FirstName{ get; set; } = default!;
+        public string Lastname{ get; set; } = default!;
+        public string Address{ get; set; } = default!;
+        public DateTime DateOfBirth{ get; set;}
+        public string TelephoneNumber{ get; set; } = default!;
+
+        public virtual LibraryCard LibraryCard { get; set;}
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
+    }
+}
